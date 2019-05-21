e1e4ee06a1ed0dc85d7591ceb1a5e0289ef28ddb
snipzer <l.arif@laposte.net>
2019-05-21 16:58:56 +0200

    Debut de prediction

--- executions/learning.js
+++ executions/learning.js
@@ -1,10 +1,53 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
 const csv = require('csvtojson');
 
 const pathToLeaningFile = "./data/training_set_final.csv";
+const pathToTestSet = "./data/test_set_final.csv";
 
 const execute = async () => {
-    const LearningData = await csv({ output: 'csv' }).fromFile(pathToLeaningFile);
-    console.log(LearningData[0]);
+    try {
+        const datas = await csv({ output: 'csv' }).fromFile(pathToLeaningFile);
+        const tests = await csv({ output: 'csv' }).fromFile(pathToTestSet);
+
+        const labels = [];
+        datas.forEach(element => {
+            labels.push([].push(element.pop()));
+        });
+
+        const model = tf.sequential({
+            layers: [
+                tf.layers.dense({batchInputShape: [,datas[0].length], units: 1, activation: 'relu'})
+            ]
+        });
+
+
+        model.compile({
+            optimizer: 'sgd',
+            loss: 'meanSquaredError',
+            metrics: ['accuracy']
+        });
+
+        function onBatchEnd(batch, logs) {
+            console.log('Accuracy', logs.acc);
+        }
+
+        const xs = tf.tensor2d(datas, [datas.length, datas[0].length]);
+        const ys = tf.tensor1d(labels);
+
+        model.fit(xs, ys, {
+            epochs: 5,
+            batchSize: datas.length,
+            callbacks: {onBatchEnd}
+        }).then(info => {
+            // console.log('Final accuracy', info.history.acc);
+            model.predict(tf.tensor(tests, [tests.length, tests[0].length]), {
+                batchSize: tests.length
+            }).print();
+        }).catch(err => console.log(err));
+    } catch(err) {
+        console.log(err);
+    }
 };
 
 execute();
